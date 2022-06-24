0cf564be7ca59b9f77cecd4bb5654149047fccae
Rakib-010 <107453168+Rakib-010@users.noreply.github.com>
2022-06-24 11:13:02 +0600

    java calculator & string operation

---
+++ OOP_010_V0.2/StringMain.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringmain.java;
+
+/**
+ *
+ * @author SARK
+ */
+public class StringMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  //      Scanner s = new Scanner(System.in);
+		StringOperation operation = new StringOperation();
+		
+//		System.out.println("Enter a string to start the operation: ");
+		
+//		String str = s.nextLine();
+		String str = "Hello java";
+		
+		operation.charAt(str);
+		
+		operation.chars(str);
+		
+		operation.codePointAt(str);
+		
+		operation.codePointBefore(str);
+		
+		operation.codePointCount(str);
+		
+		operation.codePoints(str);
+		
+		operation.compareTo(str);
+		
+		operation.compareToIgnoreCase(str);
+		
+		operation.concat(str);
+		
+		operation.contains(str);
+		
+		operation.contentEquals(str);
+		
+		operation.describeConstable(str);
+		
+		operation.endsWith(str);
+		
+		operation.equalsIgnoreCase(str);
+		
+		operation.getBytes(str);
+		
+		operation.getBytes2(str);
+		
+		operation.hashCode(str);
+		
+		operation.indent(str);
+		
+		operation.indexOf(str);
+		
+		operation.indexOf1(str);
+		
+		operation.intern(str);
+		
+		operation.isBlank(str);
+		
+		operation.isEmpty(str);
+		
+		operation.lastIndexOf(str);
+		
+		operation.lastIndexOf1(str);
+		
+		operation.length(str);
+		
+		operation.repeat(str);
+		
+		operation.replace(str);
+		
+		operation.strip(str);
+		
+		operation.stripIndent(str);
+		
+		operation.stripLeading(str);
+		
+		operation.stripTrailing(str);
+		
+		operation.substring(str);
+		
+		operation.toLowerCase(str);
+		
+		operation.toUpperCase(str);
+		
+		operation.trim(str);
+		
+
+	}
+
+}
+    }
+    
+}
